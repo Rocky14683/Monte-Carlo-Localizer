72371ee870e8152d298fb9021b5e0bb9bfb530a8
Rocky14683 <rocky10208@gmail.com>
2024-12-26 15:17:39 +0800

    reorganize environment

--- include/world.hpp
+++ include/world.hpp
@@ -1,9 +1,14 @@
 #pragma once
-namespace world {
-// Landmarks
-inline double landmarks[8][2] = {{20.0, 20.0}, {20.0, 80.0}, {20.0, 50.0}, {50.0, 20.0},
-                          {50.0, 80.0}, {80.0, 80.0}, {80.0, 20.0}, {80.0, 50.0}};
-// Map size in meters
-inline double world_size = 100.0;
+#include "particle.hpp"
 
+namespace world {
+constexpr inline size_t n_landmarks = 8;
+
+inline std::array<Point, n_landmarks> landmarks = {
+    {{20.0, 20.0}, {20.0, 80.0}, {20.0, 50.0}, {50.0, 20.0}, {50.0, 80.0}, {80.0, 80.0}, {80.0, 20.0}, {80.0, 50.0}}};
+
+// Map size in meters [0, 100] X [0, 100]
+constexpr inline double world_size = 100.0;
+
+constexpr inline double wall_size = 1.0;
 }; // namespace world
